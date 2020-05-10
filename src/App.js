bc94a10d73fc2a79f34a3383dad8c649a85bba57
Yuvraj <yuvraj17188@gmail.com>
2020-05-11 00:34:42 +0530

    contacts app with ContactsAPI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import ListContacts from './ListContacts'
 import './App.css';
+import * as ContactsAPI from './utils/ContactsAPI'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    contacts: []
+  };
+  
+  componentDidMount() {
+    ContactsAPI.getAll().then((contacts) => {
+      this.setState(() => ({
+        contacts
+      }))
+    })
+  }
+
+  deleteContact = (contactToDelete) => {
+        this.setState((prevState) => ({
+          contacts: prevState.contacts.filter(contact => contact.id !== contactToDelete.id)  
+        }));
+        ContactsAPI.remove(contactToDelete);
+  }
+
+  render() {
+    return (
+        <div>
+        <ListContacts contacts={this.state.contacts} onClick={this.deleteContact}/>
+      </div>
+    );
+  }
 }
 
 export default App;
